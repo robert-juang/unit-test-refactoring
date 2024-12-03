9c1577ed3983dea2590580bde6c2cce86fdf34dc
robert-juang <zdjiang20001225@gmail.com>
2024-12-02 22:28:22 -0500

    update directory_parser

--- directory_parser.py
+++ directory_parser.py
@@ -3,15 +3,20 @@ import os
 from utils import * 
 from errors import * 
 from helper import * 
+from constants import * 
+
+ignored_file = IGNORED_FILE 
 
 class FileNode: 
-    def __init__(self, path: str):
+    def __init__(self, path: str, 
+                 source_dir: str="src"):
         if (not os.path.isfile(path)): 
             raise FileExistsError
         self.type: str = "file" 
         self.path: str = path 
         self.name: str = os.path.basename(path) 
-        self.unit_test_path: str = self._get_unit_test_path(self.path, self.name) 
+        self.unit_test_path: str = self._get_unit_test_path(self.path, self.name, source_dir) 
+        self.unit_test_path_without_file: str = os.path.dirname(self.unit_test_path) 
         self.size: str = "" #TODO: use module to get 
         self.file_content_modified: str = ""
         self.past_file_content: list[str] = [] #tracks past version of unit test in case we want to revert (maybe use dict bc i think we should only use 5 so we don't have ridiculous memory)
@@ -23,18 +28,51 @@ class FileNode:
             logging.error("Error when reading file: ",e) 
             self.file_content_original = ""
 
-    def _get_unit_test_path(current_path, file_name): 
-        # TODO Figure this shit out 
-        intermediate_path = "" #TODO: subtract the path below from current_path 
-        return os.path.join("/Users/zhidongjiang/Desktop/unit-test-refactoring", intermediate_path, f"test_{file_name}")
+    def _get_unit_test_path(self, 
+                            current_path: str, 
+                            file_name: str, 
+                            source_dir: str): 
+        """Get directory, replace src with test, then attach test filename
+        
+        BUG: Might cause an issue with multiple src folders being replaced
+        """
+        intermediate_path = os.path.dirname(current_path) 
+
+        intermediate_path = intermediate_path.replace(source_dir, "test")
+        
+        return os.path.join(intermediate_path, f"test_{file_name}")
+    
+    def _postprocess(self, gpt_result): 
+        """TODO: edit as see fit"""
+        return gpt_result 
 
     def unit_test(self): 
-        """ Run unit test in the file. Return the unit tested code"""
-        pass 
+        """ Run unit test in the file. Return the unit tested code
+        TODO: probbaly use could use some code from aisuite 
+        """ 
+        
+        query = "..." 
+
+        #unit_test = await self.gpt_module(query=query) 
+        
+        unit_test = "print('hi')" 
+
+        unit_test = self._postprocess(unit_test) 
+
+        return unit_test 
+
 
     def refactor(self): 
-        """Refactor the code in this file. Return the refactored code"""
+        """Refactor the code in this file. Return the refactored code
+        """
+        query = "..." 
 
+        #unit_test = await self.gpt_module(query=query) 
+        refactored = "" 
+        
+        refactored = self._postprocess(refactored) 
+
+        return refactored 
 
 class DirectoryNode: 
     def __init__(self, path: str):
@@ -145,7 +183,6 @@ class DirectoryParser:
         """Scan through the entire tree and if filename match return node of file otherwise return None
         
         TODO: Maybe can optimize with a faster tree ?? Also add error cehcking
-
         """
 
         for node in dir_node.directory_content: 
@@ -160,33 +197,67 @@ class DirectoryParser:
             
         return None 
     
-    def retrieve_node_by_filename(self, file_name): 
+    def retrieve_node_by_filename(self, file_name: str): 
         """Scan thorugh the entire tree and return matching node by file name. If duplicate, raise error"""
         result = self._retrieve_file(self.directory_tree.root, file_name)
         
         return result if result else None 
-    
-    def _generate_and_write_files(self, 
+
+    def _recurse_directory(self, 
                             root: DirectoryNode, 
-                            ignore_items: list[str]): 
+                            ignore_items: list[str]=IGNORED_FILE): 
         """
-        Generate and write test files for root. Use file_content_original for the test
+        Generate and write test files for root. Use file_content_original when writing test
         """
         if not root: 
             raise DirectoryNodeNotExistError
 
-        for node in root.directory_content: 
-            print("Processing, ", node.name) 
+        for node in root.directory_content:             
             path_to_node: str = os.path.join(root.path, node.name)
 
+            print(f"Ignored {node.name}") if node.name in IGNORED_FILE else None
+
             if os.path.isdir(path_to_node):
-                self._write_folder_all(node, ignore_items)
+                print(f"||| Opening directory {node.name} |||")
+                self._recurse_directory(node, 
+                                        ignore_items)
 
-            if os.path.isfile(path_to_node): 
-                os.makedirs(node.unit_test_path, exist_ok=True)
+            if os.path.isfile(path_to_node) and node.name not in IGNORED_FILE: 
+                print(f"Processing {node.type}, ", node.name)
 
-                write_file(node.unit_test_path, 
-                           node.name,
-                           node.file_content_original) 
+                try: 
+                    unit_test = node.unit_test()
+                except Exception as e: 
+                    print("Error when generating unit test: ", e)
+                    print("Skipping file", node.name)
+                    continue 
+
+                os.makedirs(node.unit_test_path_without_file, exist_ok=True)
+
+                write_file(path=node.unit_test_path, 
+                           filename=node.name,
+                           content=unit_test) 
                 
+    def generate_tests_and_write(self, 
+                                 ignore_items: list[str]=IGNORED_FILE): 
+        """Wrapper for recursing directory and generaating tests"""
+        try: 
+            self._recurse_directory(root=self.directory_tree.root, 
+                                    ignore_items=ignore_items) 
+        except: 
+            raise UnitTestGenerationError        
     #TODO: get different versions to populate shit 
+
+    def is_test_running(self): 
+        """ 
+        TODO: tests if all tests are passing automatically 
+        """
+        pass 
+
+
+    def refactor_existing_test(self, filename): 
+        """ 
+        Refactor a file in the test folder with the filename specified like "test_app.py" 
+        """
+        
+        pass 
